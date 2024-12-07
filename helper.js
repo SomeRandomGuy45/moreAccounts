e7809c4949863dcc0087e56b08a076ed989e1d98
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2024-12-06 18:39:49 -0600

    ok bro

--- helper.js
+++ helper.js
@@ -3,5 +3,4 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       console.log('Message from content script:', message.message);
       sendResponse({ status: 'Updated cookie' });
     }
-  });
-  
+});
